7211bd5ce78fb3ad95d47f74b741f3c00516bee7
KuroRyu14 <Genecent.coc@gmail.com>
2024-06-11 00:29:52 +0800

    qwe

--- src/boot/axios.js
+++ src/boot/axios.js
@@ -2,11 +2,11 @@
 import { boot } from "quasar/wrappers";
 import axios from "axios";
 
-const api = axios.create({ baseURL: "http://192.168.1.69/" }); // Use your PHP server IP
+const api = axios.create({ baseURL: "http://192.168.1.69/EMR/backend/" });
 
 export default boot(({ app }) => {
-  app.config.globalProperties.$axios = axios; // Make axios available throughout your app
-  app.config.globalProperties.$api = api; // Dedicated API interaction
+  app.config.globalProperties.$axios = axios;
+  app.config.globalProperties.$api = api;
 });
 
 export { api };
